6fad34be35c736414d1346469957376439eacb31
Michael Totty <mictott@gmail.com>
2024-08-20 14:41:09 -0400

    xenium breastcancer QC plots across QC methods for 10x labels and Banksy clusters

--- code/sfe_xenium.R
+++ code/sfe_xenium.R
@@ -10,6 +10,7 @@ library(scater)
 library(SpatialFeatureExperiment)
 library(SFEData)
 library(ggVennDiagram)
+library(RColorBrewer)
 
 
 plot_dir <- here("plots","xenium")
@@ -37,6 +38,7 @@ sfe
 # Graphs:
 #   sample01:
 
+# ======== Convert to SpatialExperiment ========
 sfe$xcoord <- spatialCoords(sfe)[,1]
 sfe$ycoord <- spatialCoords(sfe)[,2]
 
@@ -47,9 +49,51 @@ spe <- SpatialExperiment(assay=counts(sfe),
 rowData(spe) <- rowData(sfe)
 counts(spe) <- assay(sfe)
 
+# ========= Read in cell type annotations from 10x ========
+# read in excel
+library(readxl)
+
+# read in the excel file - sheet "Xenium R1 Fig1-5 (supervised)"
+file <- here("processed-data","Cell_Barcode_Type_Matrices.xlsx")
+cell_barcode_type <- readxl::read_excel(file, sheet="Xenium R2 Fig1-5 (supervised)")
+
+# add Cluster to colData based on matching Barcode
+spe$Cluster <- cell_barcode_type$Cluster[match(spe$Barcode, cell_barcode_type$Barcode)]
+
+# drop single NA cell
+spe <- spe[,!is.na(spe$Cluster)]
+
+
+# ========= Plotting clusters =========
+# unique palette for clusters
+cluster_pal <- colorRampPalette(brewer.pal(9, "Set1"))(length(unique(spe$Cluster)))
+png(here(plot_dir, "breastcancer_spotplot_clusters.png"), width=12, height=10, units="in", res=300)
+make_escheR(spe) |>
+  add_ground(var="Cluster", point_size=.3) +
+  scale_color_manual(values=cluster_pal)
+dev.off()
+
+# ========= QC metrics =========
 png(here(plot_dir, "breastcancer_spotplot_qc_metrics.png"), width=15, height=7.5, units="in", res=300)
 p1 <- plotQC(spe, metric="nCounts", point_size=.7)
 p2 <- plotQC(spe, metric="nGenes", point_size=.7)
+p1+p2
+dev.off()
+
+spe$nCounts_log <- log1p(spe$nCounts)
+png(here(plot_dir, "breastcancer_violin_qc_metrics.png"), width=6, height=9, units="in", res=300)
+p1 <- plotColData(spe, x="Cluster", y="nCounts_log", color_by="Cluster") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(legend.position="none") +
+  labs(title="Library Size",
+       x="Cell Type") +
+  coord_flip()
+p2 <- plotColData(spe, x="Cluster", y="nGenes", color_by="Cluster") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(legend.position="none") +
+  labs(title="Num Genes",
+       x=NULL) +
+  coord_flip()
 p1+p2
 dev.off()
 
@@ -58,18 +102,35 @@ dev.off()
 spe <- localOutliers(spe, metric="nCounts", direction="lower", log=TRUE)
 spe <- localOutliers(spe, metric="nGenes", direction="lower", log=TRUE)
 
-png(here(plot_dir, "breastcancer_spotplot_discard_spotsweeper.png"), width=15, height=7.5, units="in", res=300)
+png(here(plot_dir, "breastcancer_spotplot_discard_spotsweeper_newSamples.png"), width=15, height=7.5, units="in", res=300)
 p1 <- plotQC(spe, metric="nCounts_log", outliers="nCounts_outliers", point_size=1)
 p2 <- plotQC(spe, metric="nGenes_log", outliers="nGenes_outliers", point_size=1)
 p1+p2
 dev.off()
 
+png(here(plot_dir, "breastcancer_violin_discard_spotsweeper.png"), width=8, height=9, units="in", res=300)
+p1 <- plotColData(spe, x="Cluster", y="nCounts_log", color_by="nCounts_outliers") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_color_manual(values=c("TRUE"="red", "FALSE"="grey")) +
+  theme(legend.position="none") +
+  labs(title="Library Size",
+       x="Cell Type") +
+  coord_flip()
+p2 <- plotColData(spe, x="Cluster", y="nGenes", color_by="nGenes_outliers") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_color_manual(values=c("TRUE"="red", "FALSE"="grey")) +
+  #theme(legend.position="none") +
+  labs(title="Num Genes",
+       x=NULL) +
+  coord_flip()
+p1+p2
+dev.off()
 
 
 
 # ======== Scater qc =======
 
-# arbitrary thresholding
+# ====== arbitrary thresholding ======
 spe$nCounts_discard_threshold <- spe$nCounts < 25
 spe$nGenes_discard_threshold  <- spe$nGenes < 10
 
@@ -83,8 +144,27 @@ p2 <- plotQC(spe, metric="nGenes_log", outliers="nGenes_discard_threshold", poin
 p1+p2
 dev.off()
 
+png(here(plot_dir, "breastcancer_violin_discard_threshold.png"), width=8, height=9, units="in", res=300)
+p1 <- plotColData(spe, x="Cluster", y="nCounts_log", color_by="nCounts_discard_threshold") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_color_manual(values=c("TRUE"="red", "FALSE"="grey")) +
+  theme(legend.position="none") +
+  labs(title="Library Size",
+       x="Cell Type") +
+  coord_flip()
+p2 <- plotColData(spe, x="Cluster", y="nGenes", color_by="nGenes_discard_threshold") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_color_manual(values=c("TRUE"="red", "FALSE"="grey")) +
+  #theme(legend.position="none") +
+  labs(title="Num Genes",
+       x=NULL) +
+  coord_flip()
+p1+p2
+dev.off()
 
-# 3 MAD
+
+
+# ====== 3 MAD ======
 spe$nCounts_discard_3mad <- isOutlier(spe$nCounts, nmads=3, type="lower", log=TRUE)
 spe$nGenes_discard_3mad <- isOutlier(spe$nGenes, nmads=3, type="lower", log=TRUE)
 
@@ -97,6 +177,27 @@ p2 <- plotQC(spe, metric="nGenes_log", outliers="nGenes_discard_3mad", point_siz
 
 p1+p2
 dev.off()
+
+
+png(here(plot_dir, "breastcancer_violin_discard_3mad.png"), width=8, height=9, units="in", res=300)
+p1 <- plotColData(spe, x="Cluster", y="nCounts_log", color_by="nCounts_discard_3mad") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_color_manual(values=c("TRUE"="red", "FALSE"="grey")) +
+  theme(legend.position="none") +
+  labs(title="Library Size",
+       x="Cell Type") +
+  coord_flip()
+p2 <- plotColData(spe, x="Cluster", y="nGenes", color_by="nGenes_discard_3mad") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_color_manual(values=c("TRUE"="red", "FALSE"="grey")) +
+  #theme(legend.position="none") +
+  labs(title="Num Genes",
+       x=NULL) +
+  coord_flip()
+p1+p2
+dev.off()
+
+
 
 
 # ========= Violin plots ==========
@@ -179,18 +280,90 @@ dev.off()
 
 spe$banksy_clusters <- spe$clust_M0_lam0.8_k50_res0.8
 
-png(here(plot_dir, "breastcancer_banksy_QC_metrics.png"), width=6, height=6, units="in", res=300)
-p1 <- plotColData(spe, x="banksy_clusters", y="nCounts_log", color="banksy_clusters")
-p2 <- plotColData(spe, x="banksy_clusters", y="nGenes_log", color="banksy_clusters")
-p1/p2
+# violin plots
+png(here(plot_dir, "breastcancer_banksy_violins_discard_threshold.png"), width=8, height=9, units="in", res=300)
+p1 <- plotColData(spe, x="clust_M0_lam0.8_k50_res0.8", y="nCounts_log", color_by="nCounts_discard_threshold") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_color_manual(values=c("TRUE"="red", "FALSE"="grey")) +
+  theme(legend.position="none") +
+  labs(title="Library Size",
+       x="Cell Type") +
+  coord_flip()
+p2 <- plotColData(spe, x="clust_M0_lam0.8_k50_res0.8", y="nGenes", color_by="nGenes_discard_threshold") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_color_manual(values=c("TRUE"="red", "FALSE"="grey")) +
+  #theme(legend.position="none") +
+  labs(title="Num Genes",
+       x=NULL) +
+  coord_flip()
+p1+p2
 dev.off()
 
 
-# read in excel
-library(readxl)
+png(here(plot_dir, "breastcancer_banksy_violins_discard_3mad.png"), width=8, height=9, units="in", res=300)
+p1 <- plotColData(spe, x="clust_M0_lam0.8_k50_res0.8", y="nCounts_log", color_by="nCounts_discard_3mad") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_color_manual(values=c("TRUE"="red", "FALSE"="grey")) +
+  theme(legend.position="none") +
+  labs(title="Library Size",
+       x="Cell Type") +
+  coord_flip()
+p2 <- plotColData(spe, x="clust_M0_lam0.8_k50_res0.8", y="nGenes", color_by="nGenes_discard_3mad") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_color_manual(values=c("TRUE"="red", "FALSE"="grey")) +
+  #theme(legend.position="none") +
+  labs(title="Num Genes",
+       x=NULL) +
+  coord_flip()
+p1+p2
+dev.off()
 
-# read in the excel file - sheet "Xenium R1 Fig1-5 (supervised)"
-file <- here("processed-data","Cell_Barcode_Type_Matrices.xlsx")
-cell_barcode_type <- readxl::read_excel(file, sheet="Xenium R1 Fig1-5 (supervised)")
 
-spe$annotation_10x<- cell_barcode_type$Cluster
+png(here(plot_dir, "breastcancer_banksy_violins_discard_spotsweeper.png"), width=8, height=9, units="in", res=300)
+p1 <- plotColData(spe, x="clust_M0_lam0.8_k50_res0.8", y="nCounts_log", color_by="nCounts_outliers") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_color_manual(values=c("TRUE"="red", "FALSE"="grey")) +
+  theme(legend.position="none") +
+  labs(title="Library Size",
+       x="Cell Type") +
+  coord_flip()
+p2 <- plotColData(spe, x="clust_M0_lam0.8_k50_res0.8", y="nGenes", color_by="nGenes_outliers") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_color_manual(values=c("TRUE"="red", "FALSE"="grey")) +
+  #theme(legend.position="none") +
+  labs(title="Num Genes",
+       x=NULL) +
+  coord_flip()
+p1+p2
+dev.off()
+
+
+
+# ======= UMAPs =======
+
+plotReducedDim(spe, dimred="PCA_M0_lam0.8", color_by="clust_M0_lam0.8_k50_res0.8")
+
+spe <- runUMAP(spe, assay.type="counts", ncomponents = 30, ntop = 500)
+
+png(here(plot_dir, "breastcancer_umap_banksy_clusters.png"), width=5, height=5, units="in", res=300)
+plotReducedDim(spe, dimred="UMAP", color_by="clust_M0_lam0.8_k50_res0.8")
+dev.off()
+
+png(here(plot_dir, "breastcancer_umap_banksy_nCounts.png"),  width=5, height=5, units="in", res=300)
+plotReducedDim(spe, dimred="UMAP", color_by="nCounts_log")
+dev.off()
+
+png(here(plot_dir, "breastcancer_umap_banksy_nCounts_threshold.png"),  width=5, height=5, units="in", res=300)
+plotReducedDim(spe, dimred="UMAP", color_by="nCounts_discard_threshold") +
+  scale_color_manual("color", values=c("TRUE"="red", "FALSE"="grey"))
+dev.off()
+
+png(here(plot_dir, "breastcancer_umap_banksy_nCounts_3mad.png"),  width=5, height=5, units="in", res=300)
+plotReducedDim(spe, dimred="UMAP", color_by="nCounts_discard_3mad")+
+  scale_color_manual("color", values=c("TRUE"="red", "FALSE"="grey"))
+dev.off()
+
+png(here(plot_dir, "breastcancer_umap_banksy_nCounts_spotsweeper.png"),  width=5, height=5, units="in", res=300)
+plotReducedDim(spe, dimred="UMAP", color_by="nCounts_outliers")+
+  scale_color_manual("color", values=c("TRUE"="red", "FALSE"="grey"))
+dev.off()
